5876a53b73f6372fb90b2dd721b17c9374479963
yusuf <0yusufokur0>
2021-11-07 13:29:48 +0300

    added splash screen

---
+++ app/src/main/java/com/resurrection/imkb/ui/SplashActivity.kt
@@ -0,0 +1,65 @@
+package com.resurrection.imkb.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.resurrection.imkb.R
+import com.resurrection.imkb.util.changeStatusBarColor
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.lifecycle.lifecycleScope
+import com.resurrection.imkb.databinding.ActivitySplashBinding
+import com.resurrection.imkb.ui.base.BaseActivity
+import com.resurrection.imkb.ui.main.MainActivity
+import kotlinx.coroutines.*
+
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : BaseActivity<ActivitySplashBinding>() {
+    override fun getLayoutRes(): Int = R.layout.activity_splash
+
+    override fun init(savedInstanceState: Bundle?) {
+        this.changeStatusBarColor(R.color.black)
+
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout, i: Int, i1: Int) {}
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout,
+                i: Int,
+                i1: Int,
+                v: Float
+            ) { }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout, i: Int) {
+                if (motionLayout.currentState == R.id.end){
+                    runBlocking {
+                        binding.progressBar.visibility = View.VISIBLE
+                        delay(1000)
+                        startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                    }
+                }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout,
+                i: Int,
+                b: Boolean,
+                v: Float
+            ) { }
+        })
+
+        lifecycleScope.launch (Dispatchers.IO){
+            delay(500)
+            withContext(Dispatchers.Main){
+                binding.motionLayout.transitionToEnd()
+            }
+        }
+    }
+
+
+}
+
--- app/src/main/java/com/resurrection/imkb/util/Util.kt
+++ app/src/main/java/com/resurrection/imkb/util/Util.kt
@@ -6,9 +6,12 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.resurrection.imkb.R
 
 
@@ -71,4 +74,9 @@ fun Any.isValid(): Boolean {
 fun View.setCustomAnimation(anim:Int = R.anim.fall_down) {
     this.startAnimation(AnimationUtils.loadAnimation(this.context,anim))
 }
-
+fun Activity.changeStatusBarColor(color:Int = R.color.black){
+    val window: Window = this.window
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    window.statusBarColor = ContextCompat.getColor(this, color)
+}
